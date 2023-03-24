7973212095dc717d89bbf653ccf0138f165eecad
GrigorievAnton <128778590+GrigorievAnton@users.noreply.github.com>
2023-03-24 12:12:13 +0400

    Add files via upload

--- Variables.java
+++ Variables.java
@@ -1,23 +1,34 @@
+import java.util.Scanner;
 public class Variables {
     public static void main(String[] args) {
+        // Домашнее задание
+        Scanner scan = new Scanner(System.in);
 
-        int i1 = 5;
-        int i2 = 11;
-        double d1 =5.5;
-        double d2 = 1.3;
-        long l = 20L;
-        double result = 0;
-        result = i2/d1 + d2 % i1 -l;
-        System.out.println(result);
-        double x1 = 1.1;
-        double y1 = 1;
-        result = (x1*(8.3-y1)/x1-y1)*(x1+y1);
-        System.out.println(result);
-        int a1 = 5;
-        int b = 8;
-        int result1 = a1-- - --a1 + ++a1 + a1++ + a1;
-        result = ++b - b ++ + ++b - --b;
-        System.out.println(result1);
-        System.out.println(result);
+            System.out.println("Введите целое число");
+
+        int x = scan.nextInt();
+        if (x % 2 == 0)
+            System.out.println("Число четное");
+        else
+            System.out.println("Число нечетное");
+
+        Scanner scan1 = new Scanner(System.in);
+        {
+            System.out.println("Введите сумму вклада (в рублях), а далее количество месяцев");
+        }
+        double y = scan.nextDouble();
+        int x1 = scan.nextInt();
+        double proc = 7 / 100.0;
+        while(x1 > 0) {
+            y = y + y * proc;
+            x1--;
+            System.out.println(y);
+        }
+           System.out.println(y);
     }
 }
+
+
+
+
+
